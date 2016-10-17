0e125909630d6eb54143c973b58a2bae34e80698
Maksym <Maksym>
2016-10-17 04:26:23 +0300

    HTTP methods and headers

--- router.js
+++ router.js
@@ -1,19 +1,33 @@
 var Profile = require('./profile.js');
 var renderer = require('./renderer.js');
+var querystring = require('querystring');
+
+var commonHeaders = {'Content-Type': 'text/html'};
 
 //Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   //if url == '/' && GET
   if(request.url === '/') {
-    //show search
-    response.setHeader('Content-Type', 'text/plain');
-    renderer.view('header', {}, response);
-    renderer.view('search', {}, response);
-    renderer.view('footer', {}, response);
-    response.end();
+    if(request.method.toLowerCase() === 'get') {
+      //show search
+      response.writeHead(200, commonHeaders);
+      renderer.view('header', {}, response);
+      renderer.view('search', {}, response);
+      renderer.view('footer', {}, response);
+      response.end();
+    } else {
+      //if url == '/' && POST
+
+      // get post data from body
+      request.on('data', function(postBody) {
+        // extract the username
+        var query = querystring.parse(postBody.toString());
+        response.writeHead(303, {'Location': '/' + query.username});
+        response.end();
+        //redirect to /:username
+      });
+    }
   }
-  //if url == '/' && POST
-    //redirect to /:username
 }
 
 //Handle HTTP route GET /:username i.e. /chalkers
@@ -21,7 +35,7 @@ function user(request, response) {
   //if url == '/....'
   var userName = request.url.replace('/', '');
   if(userName.length > 1) {
-    response.setHeader('Content-Type', 'text/html');
+    response.writeHead(200, commonHeaders);
     renderer.view('header', {}, response);
 
     //get json from Treehouse
